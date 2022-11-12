258453de36bf4198a99b34f3f944aa69a8276145
avantgardeee <krist.andrey1@gmail.com>
2022-11-12 21:39:13 +0300

    lesson 56

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import Friends from "./components/Friends/Friends";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
 import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props) => {
